d29b06934d8f7f21be96d46359e604c9ec355122
Dharmender <dharmenderarya477@gmail.com>
2023-11-25 21:49:43 +0530

    Masai Evaluation

---
+++ Masai_Doctor_Records/script.js
@@ -0,0 +1,9 @@
+const submit = document.getElementById('submit-btn');
+const name = document.getElementById("name");
+const name = document.getElementById("name");
+const name = document.getElementById("name");
+const name = document.getElementById("name");
+
+submit.addEventListener('click' , () => {
+    
+})
